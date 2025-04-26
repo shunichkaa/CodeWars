1f4cb3830bac4f8b3cf88b9447c581c065bdc962
Aleksandra Mate <matealeksandra@gmail.com>
2025-04-26 22:13:17 +0200

    commit

---
+++ Who likes it?.js
@@ -0,0 +1,30 @@
+// You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+//
+// 	Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+//
+// 	[]                                -->  "no one likes this"
+// 		["Peter"]                         -->  "Peter likes this"
+// 		["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// 		["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// 		["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+// Note: For 4 or more names, the number in "and 2 others" simply increases.
+//
+
+const LIKE_TEMPLATES = {
+	0: 'no one likes this',
+	1: '{first} likes this',
+	2: '{first} and {second} like this',
+	3: '{first}, {second} and {third} like this',
+	default: '{first}, {second} and {count} others like this'
+};
+
+function likes(names) {
+	const count = names.length;
+	const template = LIKE_TEMPLATES[count] || LIKE_TEMPLATES.default;
+
+	return template
+	.replace('{first}', names[0] || '')
+	.replace('{second}', names[1] || '')
+	.replace('{third}', names[2] || '')
+	.replace('{count}', count - 2);
+}
